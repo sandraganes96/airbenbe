5b8f88fe1af5dd46f6389b6c43a9ed4ed926957d
nfladin <naufal.adin12@gmail.com>
2019-04-21 14:04:16 +0700

    login

--- application/views/login.php
+++ application/views/login.php
@@ -1,258 +1,272 @@
-  <style>
-  #nav>li>a, #nav>li>div>a {
-    color: black;
-    margin: 12px;
-    font-family: Cereal-M;
-    font-size: 14px;
-  }
-  #nav>li:hover {
-    border-bottom: 2px solid black;
-  }
-  #nav {
-    margin-right: -22px;
-  }
-  .register{
-    text-decoration: none;
-    display: block;
-    padding: 15px;
-    color: white;
-    transition: color 1000ms;
-  }
-  .register:hover{
-    color: black;
-  }
-  .modal_container{
-    width:100%;
-    height: 90%;
-    position: fixed;
-    background:transparent;
-    opacity: 0;
-    font-family: sans-serif;
-    pointer-events: none;
-  }
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" href="<?php echo base_url(); ?>assets/img/logo.png"/>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<title>Log In / Sign Up to Airbenbe</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
+	<script src="<?php echo base_url(); ?>assets/jquery-3.3.1.min.js"></script>
+	<script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+	<style type="text/css">
+		@font-face {
+			font-family: "Cereal-M";
+			src: url("<?php echo base_url() ?>assets/fonts/AirbnbCereal-Medium.ttf");
+		}
+		@font-face {
+			font-family: "Cereal-L";
+			src: url("<?php echo base_url() ?>assets/fonts/AirbnbCereal-Light.ttf");
+		}
+		@font-face {
+			font-family: "Cereal-B";
+			src: url("<?php echo base_url() ?>assets/fonts/AirbnbCereal-Bold.ttf");
+		}
+		@font-face {
+			font-family: "Cereal-Bk";
+			src: url("<?php echo base_url() ?>assets/fonts/AirbnbCereal-Book.ttf");
+		}
+		.navbar.transparent {
+			padding: 15px;
 
-  .modal_container:target{
-    opacity: 1;
-    pointer-events: auto;
-  }
-  .modal{
-        height: 600px;
-        margin:auto;
-        position: absolute;
-        top:0px; bottom: 0px;
-        right: 0px; left:0px;
-        background:transparent;
+		}
+		#up {
+			background-position: 10% 50%;
+			background-size: 1350px;
+			height: 680px;
+			background-repeat: no-repeat;
 
-    }
+		}
+		#nav>a {
+			color: white;
+			margin: 13px;
+			font-family: Cereal-M;
+			font-size: 14px;
 
-    .modal_heading{
-        width: 100%;
-        display: block;
-        height: 10px;
-        margin-bottom: 30px;
-        padding: 10px;
-        text-align: center;
-        font-size: 14px;
-        font-weight: bold;
-        color:#484848;
-        border-bottom: 2px solid #fefefe;
-    }
+		}
+		#nav {
+			margin-right: -110px;
+		}
 
-    .modal-body>form>input{
-        margin-bottom: 15px;
-        padding: 10px;
-        width: 100%;
-        box-sizing: border-box;
-        border-radius: 5px;
-        height: 50px;
-        border: 1px solid;
-        font-size: 15px;
-    }
-    input:checked{
-        background:#ffb400;
-    }
+		label {
+			font-family: Cereal-M;
+			font-size: 12px;
 
-    input:focus{
-        outline-color: none;
-        border: #007a87 1px solid;
-    }
-
-    select{
-        margin-bottom: 5px;
-        padding: 10px;
-        width: 32.5%;
-        box-sizing: border-box;
-        border-radius: 5px;
-        height: 50px;
-        border: 1px solid;
-        font-size: 15px;
-    }
-    .btnRegister{
-        background: #ff5a5f;
-        color:#fff;
-        font-size: 14pt;
-        margin-top: 10px;
-        height: 50px;
-        border-radius: 5px;
-        border:1px solid #e6e6e6;
-        font-weight: bold;
-    }
-    .close{
-        color:black;
-        position: absolute;
-        background: white;
-        text-decoration: none;
-        top:20px;
-        left: 20px;
-        border-radius: 50%;
-        transition: 500ms;
-    }
-
-    .close:hover{
-        cursor: pointer;
-    }
-
-    .modal_footer{
-        display: block;
-        text-align: left;
-        font-size: 15px;
-    }
-
-    #email{
-        background-image: url(mail.png);
-        background-repeat: no-repeat;
-        background-position: right;
-    }
-
-    #depan,#belakang{
-        background-image: url(ppl.png);
-        background-repeat: no-repeat;
-        background-position: right;
-    }
-
-    #pass{
-        background-image: url(pass.png);
-        background-repeat: no-repeat;
-        background-position: right;
-    }
-  </style>
+		}
+  	</style>
 </head>
 <body>
-  <div class="modal fade" id="myModal" role="dialog">
-	    <div class="modal-dialog">
-	    <div class="modal-content">
-	        <div class="modal-header">
-	          <button type="button" class="close" data-dismiss="modal">X</button> <br><br>
-	          <span class="modal_heading">Daftar dengan
-	            <a href="#" class="FB" style="color:#007a87;text-decoration-line: none;">Facebook</a> atau
-	            <a href="#" class="FB" style="color:#007a87;text-decoration-line: none;">Google</a>
-	            <br> <p>atau</p>
-	    			</span>
-	        </div>
-	        <div class="modal-body">
-	            <form>
-	                <input id="email" type="text" placeholder="Alamat email" ><br>
-	                <input id="depan" type="text" placeholder="Nama depan" ><br>
-	                <input id="belakang" type="text" placeholder="Nama belakang" ><br>
-	                <input id="pass" type="password" placeholder="Buat kata sandi" >
-	                <p style="font-size: 11pt; font-weight: bold">Tanggal Lahir</p>
-	                <p style="font-size: 10pt;">Anda harus berusia setidaknya 18 tahun. Orang lain tidak akan melihat tanggal lahir Anda.</p>
-	                    <select name="Bulan">
-	                        <option>Bulan</option>
-	                        <option value="Januari">Januari</option>
-	                        <option value="Februari">Februari</option>
-	                        <option value="Maret">Maret</option>
-	                        <option value="April">April</option>
-	                        <option value="Mei">Mei</option>
-	                        <option value="Juni">Juni</option>
-	                        <option value="Juli">Juli</option>
-	                        <option value="Agustus">Agustus</option>
-	                        <option value="September">September</option>
-	                        <option value="Okbtober">Oktober</option>
-	                        <option value="November">November</option>
-	                        <option value="Desember">Desember</option>
-	                    </select>
-	                    <select name="Hari">
-	                        <option>Hari</option>
-	                        <option value="1">1</option>
-	                        <option value="2">2</option>
-	                        <option value="3">3</option>
-	                        <option value="4">4</option>
-	                        <option value="5">5</option>
-	                        <option value="6">6</option>
-	                        <option value="7">7</option>
-	                        <option value="8">8</option>
-	                        <option value="9">9</option>
-	                        <option value="10">10</option>
-	                        <option value="11">11</option>
-	                        <option value="12">12</option>
-	                        <option value="13">13</option>
-	                        <option value="14">14</option>
-	                        <option value="15">15</option>
-	                        <option value="16">16</option>
-	                        <option value="17">17</option>
-	                        <option value="18">18</option>
-	                        <option value="19">19</option>
-	                        <option value="10">20</option>
-	                        <option value="21">21</option>
-	                        <option value="22">22</option>
-	                        <option value="23">23</option>
-	                        <option value="24">24</option>
-	                        <option value="25">25</option>
-	                        <option value="26">26</option>
-	                        <option value="27">27</option>
-	                        <option value="28">28</option>
-	                        <option value="29">29</option>
-	                        <option value="30">30</option>
-	                        <option value="31">31</option>
-	                    </select>
-	                    <select name="Tahun">
-	                        <option>Tahun</option>
-	                        <option value="1991">1991</option>
-	                        <option value="1992">1992</option>
-	                        <option value="1993">1993</option>
-	                        <option value="1994">1994</option>
-	                        <option value="1995">1995</option>
-	                        <option value="1996">1996</option>
-	                        <option value="1997">1997</option>
-	                        <option value="1998">1998</option>
-	                        <option value="1999">1999</option>
-	                        <option value="2000">2000</option>
-	                        <option value="2001">2001</option>
-	                    </select>
-	                <p style="font-size: 10pt">Kami akan mengirimkan promo pemasaran, penawaran khusus,inspirasi, dan pembaruan kebijakan melalui email.</p>
-	                <input type="submit" class="btnRegister" value="Daftar">
-	                <p style="font-size: 10pt"> Saya tidak ingin menerima pesan pemasaran dari Airbnb. Saya juga bisa memilih untuk berhenti menerima pesan seperti
-	                ini kapan pun dengan menyetel pengaturan akun saya atau melalui tautan di pesan tersebut.</p>
-	            </form>
-	        	</div>
-	        	<div class="modal-footer">
-	            <span class="modal_footer">Sudah punya akun Airbnb?
-	                <a href="#" style="color:#007a87;text-decoration-line: none;">Log masuk</a>
-	            </span>
-						</div>
-	    </div>
+<div id="up">
+	<nav class="navbar transparent navbar-dark navbar-fixed-top">
+		<div class="container" >
+		<a style="margin-left:-180px;" class="navbar-footer" href="#" ><img style="width: 33px;" src="<?php echo base_url();?>assets/img/logo.png"></a>
+		<!-- <a style="margin-left:-650px;" class="navbar-footer" href="#" ><img style="width: 20px;" src="<?php echo base_url();?>assets/img/search.png"></a> -->
+			<div style="margin-left: -680px;">
+  				<span class="fa fa-search"></span>
+  					<input placeholder="Search">
+			</div>
+			<div id="nav">
+				<a style="padding:15px; color:black"href="#">Become a host</a>
+				<a style="padding:10px; color:black"href="#">Help</a>
+				<a style="padding:10px; color:black"href="#">Sign Up</a>
+				<a style="padding:10px; color:black"href="#">Log in</a>			
+			</div>		
+	</nav>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3">
+				<div class="card" style="width: 442px; height: 400px; padding: 15px; border-radius: 5px; margin-top: 45px; margin-left: 375px;">
+					<div></div>
+					<div style=""class="panel-body signup-login-form__extra-panel-body first">
+					<a style="background-color:#3B5998; width:375px; color:white;" href="#" class="fb btn">
+         				<i style="background-color:#3B5998; float:left; padding-top:5px; color:white;" class="fa fa-facebook fa-fw"></i> Login with Facebook
+					 </a>
+					 <a style="background-color:#white; width:375px; color:black; border:1px solid;"href="#" class="google btn">
+						 <i style="background-color:white; float:left; padding-top:5px;" class="fa fa-google fa-fw">
+          				</i> Login with Google
+        			</a>
+			<hr>
+		  <!-- <div class="signup-or-separator">
+			<span class="h6 signup-or-separator--text">or</span>
+			<hr aria-hidden="true">
+		  </div> -->
+		
+		<form style="padding:20px;" accept-charset="UTF-8" action="/authenticate" class="login-form " data-action="Signin" method="post"><div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="✓"><input name="authenticity_token" type="hidden" value="V4$.airbnb.com$3UHNINS3dqg$jpzTx_bZkypJrWvpcy7PKubbOHF7Ud5g2W152oKiRDI="></div>
+		  <input id="from" name="from" type="hidden" value="email_login">
+		  <input id="airlock_id" name="airlock_id" type="hidden" value=""> 
+			<div style="padding-bottom:7px;" class="control-group space-1">
+			 	 <input style="width:370px;" class="decorative-input inspectletIgnore" id="signin_email" name="email" placeholder="Email Address" type="email">
+			</div>
+		 	<div class="control-group space-2">
+				<input style="width:370px;" autocomplete="off" class="decorative-input inspectletIgnore" id="signin_password" name="password" placeholder="Password" type="password">
+		  	</div>
+			<div class="clearfix space-1">
+			<div class="checkbox">
+					<label><input type="checkbox" name="remember"> Remember me</label>
+					<a style="float:right; color:black;" href="#" class="forgot-password">Forgot Password?</a>
+			</div>
+		
+		  <button style="padding-bottom:10px; width:375px;  background-color:#ff5a5f; color:white; border-radius:3pt; border-color:#ff5a5f; " type="submit" >
+			Log in
+		  </button>
+		
+		</form>
+		
+		
+				  <div class="panel-body signup-login-form__extra-panel-body">
+					<div style="padding-top:20px;" class="signup-login-form__switch-copy">
+					  	Don’t have an account?
+					  	<button style="float:right; background-color:white; color:#ff5a5f; border-radius:3pt; border-color:#ff5a5f;" type="submit"  id="user-sign-btn">
+							Sign Up
+		  				</button>
+					</div>
+					
+				  </div>
+			  </div>
+				</div>
+			</div>
+			<div class="col-md-3">
+			
+			</div>
+			<div class="col-md-3">
+				
 			</div>
 		</div>
+	</div>
+	<br/>
+	<br/>
+	<br/>
 </div>
-  <nav class="navbar navbar-expand-lg navbar-light">
-		<div class="container-fluid" >
-			<div class="navbar-header">
-				<a class="navbar-brand" href="<?php echo base_url(); ?>" ><img style="width: 31px;" src="<?php echo base_url();?>assets/img/logo.png"></a>
-        <input type="text" name="" value="">
-			</div>
-			<ul class="nav navbar-nav" id="nav">
-				<li class="nav-item">
-					<div class="dropdown">
-						<a class="nav-link" data-toggle="dropdown" href="#">Menjadi tuan rumah</a>
-						<ul class="dropdown-menu">
-							<li><a href="#">lalala</a></li>
-						</ul>
-					</div>
-				</li>
-				<li class="nav-item"><a class="nav-link" href="#">Bantuan</a></li>
-				<li class="nav-item"><a class="nav-link" href="" data-toggle="modal" data-target="#myModal">Daftar</a></li>
-				<li class="nav-item"><a class="nav-link" href="<?php echo site_url('c_login/index'); ?>">Masuk</a></li>
-			</ul>
-	</nav>
+<footer  style="float:c">
+<div class="container text-center text-md-left" id='footer'>
+
+<div class="row"style="padding-left : 120px">
+
+  <br>
+  <div class="col-md-3" >
+
+	<h5 class="font-weight-bold mt-3 mb-4" id="fonttitlefooter">Airbenbr</h5>
+
+	<ul class="list-unstyled">
+	  <li>
+		<a style="color:black; " href="#!">Careers</a>
+	  </li>
+	  <li>
+		<a style="color:black;   " href="#!">Press</a>
+	  </li>
+	  <li>
+		<a style="color:black;   " href="#!">Policies</a>
+	  </li>
+	  <li>
+		<a style="color:black;   " href="#!">Help</a>
+	  </li>
+	  <li>
+		<a style="color:black;   " href="#!">Diversity & Belonging</a>
+	  </li>
+	</ul>
+
+  </div>
+
+  <div class="col-md-3 mx-auto">
+
+
+	<h5 class="font-weight-bold mt-3 mb-4" id="fonttitlefooter">Discover</h5>
+
+	<ul class="list-unstyled" >
+	  <li>
+		<a style="color:black;   " href="#!">Trust & Safety</a>
+	  </li>
+	  <li>
+		<a style="color:black;   " href="#!">Travel Credit</a>
+	  </li>
+	  <li>
+		<a style="color:black;   " href="#!">Gift Cards</a>
+	  </li>
+	  <li>
+		<a style="color:black;   " href="#!">Airbenbe Citizen</a>
+	  </li>
+	  <li>
+		<a style="color:black;   " href="#!">Business Trabel</a>
+	  </li>
+	  <li>
+		<a style="color:black;   " href="#!">Guidebooks</a>
+	  </li>
+	  <li>
+		<a style="color:black;   " href="#!">Airbenbemag</a>
+	  </li>
+	  <li>
+		<a style="color:black;   " href="#!">Events</a>
+	  </li>
+	</ul>
+
+  </div>
+  <div class="col-md-3 mx-auto">
+
+	<h5 class="font-weight-bold mt-3 mb-4" id="fonttitlefooter">Hosting</h5>
+
+	<ul class="list-unstyled">
+	  <li>
+		<a style="color:black;   " href="#!">Why Host</a>
+	  </li>
+	  <li>
+		<a style="color:black;   " href="#!">Hospitality</a>
+	  </li>
+	  <li>
+		<a style="color:black;   " href="#!">Reponsibility Hosting</a>
+	  </li>
+	  <li>
+		<a style="color:black;   " href="#!">Community Center</a>
+	  </li>
+	  <li>
+		<a style="color:black;   " href="#!">Host an Experience</a>
+	  </li>
+	  <li>
+		<a style="color:black;   " href="#!">Open Homes</a>
+	  </li>
+	</ul>
+
+  </div>
+  <div class="col-md-3 mx-auto">
+
+
+	<h5 class="font-weight-bold mt-3 mb-4" id="fonttitlefooter">Support</h5>
+
+	<ul class="list-unstyled">
+	  <li>
+		<a style="color:black;   " href="#!">Terms</a>
+	  </li>
+	  <li>
+		<a style="color:black;   " href="#!">Privacy</a>
+	  </li>
+	  <li>
+		<a style="color:black;   " href="#!">Site Map</a>
+	  </li>
+	</ul>
+
+  </div>
+  
+		</footer>
+		<hr>
+		<div id="footer">
+			<a style="padding-left:300px;" class="navbar-footer" href="#" ><img style="width: 25px;" src="<?php echo base_url();?>assets/img/logo.png"></a>
+			<a style="padding-right:475px" dir="ltr" class="_1rbmiub1">© 2019 Airbnb, Inc. All rights reserved.</a>
+			
+			<button style="right:100px; background-color:white;  border-radius:3pt; border-color:black 1px;" type="submit"  id="user-sign-btn">
+				English
+			</button>
+			<button style="right:100px; background-color:white;  border-radius:3pt; border-color:black 1px;" type="submit"  id="user-sign-btn">
+				IDR
+			</button>				  
+		</div>
+		<br>
+		<br>
+		<br>
+		
+		
+</ul>
+</div>
+</div>
+</div>
+
 </body>
+</html>
